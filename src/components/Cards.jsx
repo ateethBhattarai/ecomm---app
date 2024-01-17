4c91a7a8f4d88aafd4b2a462a1c0834a85c7e99b
ateethBhattarai <ateeth.bhattarai02@gmail.com>
2024-01-17 19:06:51 +0545

    like icon added on cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import { Heart } from "lucide-react";
 
 const Cards = ({ price, itemName, image }) => {
+  const [like, setLike] = useState(false);
+  const likeItem = () => {
+    setLike(!like);
+  };
   return (
     <div className="card card-compact w-80 bg-base-100 shadow-xl">
       <figure className=" h-40">
@@ -10,6 +15,12 @@ const Cards = ({ price, itemName, image }) => {
         <h2 className="card-title">{itemName}</h2>
         <p className="font-serif">Rs.{price}</p>
         <div className="card-actions justify-end">
+          <button className="btn" onClick={likeItem}>
+            <Heart
+              fill={like ? "red" : "none"}
+              stroke-width={like ? "0.5" : "2"}
+            />
+          </button>
           <button className="btn ">Add to Cart</button>
         </div>
       </div>
