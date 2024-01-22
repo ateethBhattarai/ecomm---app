7d0d021df47c10e4421a087bf3ff072e825ee9a5
ateethBhattarai <ateeth.bhattarai02@gmail.com>
2024-01-22 19:54:43 +0545

    manage usercontext

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -6,9 +6,14 @@ import { userData } from "../utils/userData";
 export const UserContext = createContext(null);
 
 const UserContextProvider = ({ children }) => {
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState(null);
 
-  //   login randomized user
+  /*
+        DEV NOTE:
+        To randomly select a user as the logged-in user.
+        Note: Uncomment the useEffect below to have no user selected as the logged-in user.
+    */
+
   useEffect(() => {
     const randomIndex = Math.floor(Math.random() * userData.length);
     setUser(userData[randomIndex]);
